6b87f9e30a60b2816e47d8d7df0980bdb2ea0d45
David Gardiner <david@gardiner.net.au>
2020-01-25 16:29:31 +1030

    extra output logging

--- vsts-fullbuild-ubuntu-vm/scripts/docker-vstsbuild-install.sh
+++ vsts-fullbuild-ubuntu-vm/scripts/docker-vstsbuild-install.sh
@@ -7,14 +7,18 @@ echo "ooooo      FULL INSTALL      ooooo" >> /home/$5/install.progress.txt
 
 echo "Installing prerequisites" >> /home/$5/install.progress.txt
 
+echo "apt-get update"
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates gnupg-agent
+
+echo "add-apt-repository docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
+echo "add-apt-repository git"
 sudo add-apt-repository ppa:git-core/ppa -y
 
 # Download the Microsoft repository GPG keys
@@ -23,6 +27,8 @@ wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-pr
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
 
+echo "add-apt-repository universe"
+
 # Enable the "universe" repositories
 sudo add-apt-repository universe -y
 
@@ -30,7 +36,7 @@ sudo apt-get update
 
 # Install Docker Engine and Compose
 echo "Installing Docker Engine and Compose" >> /home/$5/install.progress.txt
-
+echo "Docker"
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo service docker start
@@ -45,18 +51,18 @@ sudo /bin/date +%H:%M:%S >> /home/$5/install.progress.txt
 
 # Git
 echo "Installing Git" >> /home/$5/install.progress.txt
-
+echo "Git"
 sudo apt-get install -y git
 
 # PowerShell
 echo "Installing PowerShell Core" >> /home/$5/install.progress.txt
-
+echo "PowerShell"
 # Install PowerShell
 sudo apt-get install -y powershell
 
 # Download VSTS build agent
 echo "Downloading VSTS Build agent package" >> /home/$5/install.progress.txt
-
+echo "Azure Pipelines"
 sudo -u $5 mkdir /home/$5/downloads
 
 cd /home/$5/downloads
@@ -92,7 +98,12 @@ echo User: $5 >> /home/$5/vsts.install.log.txt 2>&1
 echo =============================== >> /home/$5/vsts.install.log.txt 2>&1
 
 echo Running Agent.Listener >> /home/$5/vsts.install.log.txt 2>&1
-sudo -u $5 -E bin/Agent.Listener configure --unattended --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool "$3" >> /home/$5/vsts.install.log.txt 2>&1
+
+# Create work directory on second disk
+sudo -mkdir /mnt/agent_work
+sudo setfacl -m user:vstsBuild:rwx /mnt/agent_work
+
+sudo -u $5 -E bin/Agent.Listener configure --unattended --replace --acceptteeeula --url $1 --auth PAT --token $2 --pool "$3" --work "/mnt/agent_work"  >> /home/$5/vsts.install.log.txt 2>&1
 echo =============================== >> /home/$5/vsts.install.log.txt 2>&1
 echo Running ./svc.sh install >> /home/$5/vsts.install.log.txt 2>&1
 sudo -E ./svc.sh install $5 >> /home/$5/vsts.install.log.txt 2>&1
